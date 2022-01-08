ca4fe86c66d7048d128cc5cdfe194853360a4ccc
WILLIAM DE SA RODRIGUES <wsr2k2@gmail.com>
2022-01-07 22:47:17 -0300

    ...

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
   
   const config = new DocumentBuilder()
     .setTitle('API-Twitter') // local onde pode ser colocado o nome do projeto
-    .setDescription('Projeto 02 do modulo 04 da Blue. O projeto consiste em criar uma API para controle do Twitter, onde o usurio postará tweets e terá seguidores.') // pequena descrição do projeto
+    .setDescription('Projeto 3 do modulo 04 da Blue. O projeto consiste em criar uma API para controle do Twitter, onde o usurio postará tweets e terá seguidores.') // pequena descrição do projeto
     .setVersion('2.0') // versão do projeto
     .addTag('Auth') // tag para agrupar os endpoints
     .addTag('Usuários') // tag para agrupar os endpoints
